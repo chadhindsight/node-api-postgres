5df9b4dac3e12aee8f4410920b1a5a0130efe4a4
chad hinds <chadrickj8@gmail.com>
2022-10-13 13:48:37 -0400

    New ting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const db = require('./queries')
-
+// New ting
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
