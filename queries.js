ed5c13210ccfd91b3807ebdddac7313b41701db0
chad hinds <chadrickj8@gmail.com>
2022-10-13 14:09:27 -0400

    Get all users fam

--- queries.js
+++ queries.js
@@ -8,7 +8,7 @@ const pool = new Pool({
     port: process.env.port
 })
 
-// Get all users
+// Get all users fam
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
         if (error) {
