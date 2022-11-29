abd09306eec77b21082b0c331f71d95c2b690a3e
chad hinds <chadrickj8@gmail.com>
2022-11-29 10:47:02 -0500

    Change var name to userID

--- queries.js
+++ queries.js
@@ -42,17 +42,17 @@ const createUser = (request, response) => {
 }
 
 const updateUser = (request, response) => {
-    const id = parseInt(request.params.id)
+    const userID = parseInt(request.params.id)
     const { name, email } = request.body
 
     pool.query(
         'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-        [name, email, id],
+        [name, email, userID],
         (error, results) => {
             if (error) {
                 throw error
             }
-            response.status(200).send(`User modified with ID: ${id}`)
+            response.status(200).send(`User modified with ID: ${userID}`)
         }
     )
 }
